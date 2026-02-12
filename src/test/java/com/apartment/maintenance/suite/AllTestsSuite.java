8e73147b4bde60f818f24f549e20379d9df65fb6
deshmukhambiii <ambika.deshmukh@cumminscollege.in>
2026-02-12 10:20:26 +0530

    Add dedicated JUnit test suite class and suite dependency

---
+++ src/test/java/com/apartment/maintenance/suite/AllTestsSuite.java
@@ -0,0 +1,9 @@
+package com.apartment.maintenance.suite;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.apartment.maintenance")
+public class AllTestsSuite {
+}
